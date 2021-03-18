cb2a91bdecc1241899eed36414d9a9107f57b847
Richard <mail@richard.yt>
2021-03-19 00:09:51 +0100

    exit if youtube-dl not installed

--- src/main/kotlin/yt/richard/marcidus/main.kt
+++ src/main/kotlin/yt/richard/marcidus/main.kt
@@ -1,8 +1,19 @@
 package yt.richard.marcidus
 
 import yt.richard.marcidus.utils.IGLoginUtils
+import kotlin.system.exitProcess
 
 suspend fun main() {
+    checkDependencies()
     val client = IGLoginUtils.login() // login
     PostScheduler(client).start() // start new postscheduler instance
+}
+
+fun checkDependencies() {
+    try {
+        Runtime.getRuntime().exec("youtube-dl")
+    } catch (e: Exception) {
+        println("youtube-dl not found. Exiting.") // TODO: disable video reposting instead of exiting
+        exitProcess(0)
+    }
 }
